d62b2ada05cd687e4ac71d8914f02ad5f22237dd
Zhidong Zhu <zhizhu@microsoft.com>
2019-06-24 11:47:18 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="zzpiptest",
+      version="0.1",
+      description="Test for pip install git+",
+      url="https://github.com/zzn2/PipPackageTest",
+      packages=find_packages(),
+      )
